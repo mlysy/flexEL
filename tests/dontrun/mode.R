93a1bbe5961b48389cb4d212c5a3e9d419d441f5
Shimeng Huang <dora.huang.sunshine@gmail.com>
2018-06-22 11:08:11 -0400

    Mode finding with nlm with gradient for mr. Prototyped in R. See donotrun/mode.R.

--- tests/dontrun/mode.R
+++ tests/dontrun/mode.R
@@ -8,10 +8,11 @@ source("../testthat/el-model.R")
 #'@param y is length-\code{nObs} vector
 #'@param theta is length-\cote{p} vector
 #'@details ...
-#'@return 
-mr.gradMat_R <- function(y, X, theta) {
-  grad <- apply(X,MARGIN = 1, function(x) sum(x^2))
-  return(grad)
+#'@return a list of gradient values of length \code{nObs}
+mr.deltaG_R <- function(y, X, theta) {
+  lx <- split(X, row(X))
+  dg <- lapply(lx, function(x) tcrossprod(x,x))
+  return(dg)
 }
 
 #'
@@ -19,8 +20,9 @@ mr.gradMat_R <- function(y, X, theta) {
 #'@param omegas is length-\code{nObs} vector
 #'@param gradmat is \code{nObs x p} matrix returned by gradMat
 #'@return value of the gradient of the logEL evaluated at theta
-logELgrad_R <- function(omegas, lambda, gradmat) {
-  return(lambda %*% gradmat %*% omegas)
+logELgrad_R <- function(omegas, lambda, gradlist) {
+  grad <- sapply(gradlist, function(x) lambda %*% x)
+  return(c(-grad %*% omegas))
 }
 
 #'
@@ -35,9 +37,9 @@ mr.neglogEL_R <- function(y, X, beta) {
   if (lout$conv) {
     lambda <- lout$lambda
     omegas <- c(1/(1-t(lambda) %*% t(G)) / sum(1/(1-t(lambda) %*% t(G))))
-    res <- -sum(log(omegas))
-    gradmat <- mr.gradMat_R(y, X, beta)
-    grad <- logELgrad_R(omegas, lambda, gradmat)
+    res <- -sum(log(omegas)) # negative logEL
+    gradlist <- mr.deltaG_R(y, X, beta)
+    grad <- -logELgrad_R(omegas, lambda, gradlist) # negative gradient
     attr(res, "gradient") <- grad
   }
   else {
@@ -63,15 +65,20 @@ mr.neglogEL_R(y,X,mu0)
 numpoints <- 100
 mu.seq <- seq(-.5+mu0,.5+mu0,length.out = numpoints)
 logel.seq <- rep(NA,numpoints)
+grad.seq <- rep(NA,numpoints)
 for (ii in 1:numpoints) {
-  logel.seq[ii] <- -mr.neglogEL_R(y,X,mu.seq[ii])
+  temp <- -mr.neglogEL_R(y,X,mu.seq[ii])
+  logel.seq[ii] <- temp
+  grad.seq[ii] <- attributes(temp)$gradient
 }
 logelmode <- plotEL(mu.seq, logel.seq, mu0, mean(y), expression(mu))
+plot(grad.seq,type='l')
+abline(h=0,col='blue')
 
-nlm(mr.logEL_R,1,y=y,X=X)
+nlm(mr.neglogEL_R,1.2,y=y,X=X)
 
 # 2-d problem
-n <- 500
+n <- 200
 p <- 2
 X0 <- matrix(rep(1,n),n,1)
 X1 <- matrix(rnorm(n),n,1)
@@ -82,13 +89,15 @@ beta_slope <- 1.5
 y <- 1 + c(X1 %*% beta_slope) + eps 
 beta0 <- c(beta_intercept, beta_slope)
 
-mr.logEL_R(y,X,beta0)
+mr.neglogEL_R(y,X,beta0)
 
-nlm(mr.logEL_R,beta0,y=y,X=X)
+nlm(mr.neglogEL_R,beta0*1.05,y=y,X=X)
 
-# ---- 
+## Note: the gradient in HMC paper only works for theta in the support of EL
+## so if the initial point is far away this may not work.
+
+# ---- try nlm ----
 negnorm <- function(x) {
-  return(-dnorm(x))
+  return(-dnorm(x, mean=1))
 }
-
-nlm(negnorm,.5)
+nlm(negnorm,1)
