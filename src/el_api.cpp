fadc54826409c70447ca0bc339ae7e84dc7c6dd8
mlysy <mlysy@users.noreply.github.com>
2020-02-25 18:12:51 -0500

    added src/el_api.cpp

---
+++ src/el_api.cpp
@@ -0,0 +1,22 @@
+/// @file el_api.cpp
+
+MeanRegModel mrG(X, y); // X = MatrixXd, y = VectorXd
+InnerEL el(mrG.get_nObs(), mrG.get_nEq()); // memory allocation
+// el contains G_, which has nObs + 1 rows (columns?) for support adjustment
+
+for(int ii=0; ii<nTheta; ii++) {
+  mrG.set_theta(Theta.col(ii));
+  // set el.G_ to mrG.evalG()
+  // method 1: this has an extra copy
+  mrG.evalG(tmpG); // R side: tmpG <- mrG_eval(X, y, Theta[,ii])
+  el.set_G(tmpG);
+  loglik[ii] = el.logEL();
+  // method 2: this exposes internal storage directly (a bit unsafe)
+  mrG.evalG(el.get_Gptr());
+  loglik[ii] = el.logEL();
+  // method 3: doesn't work well for Gadj...
+  mrG.evalG(tmpG);
+  loglik[ii] = el.logEL(tmpG);
+}
+
+// can we do something similar with InnerELC?
