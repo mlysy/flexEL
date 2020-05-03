e1d9f723f17cac3d9552bd1008b186fdc5688283
Shimeng Huang <dora.huang.sunshine@gmail.com>
2020-05-02 22:57:27 -0400

    Added example.

--- R/omega_hat_EM_smooth.R
+++ R/omega_hat_EM_smooth.R
@@ -1,4 +1,4 @@
-#' Returns the empirical distribution, omegas, from empirical likelihood (EL) maximization.
+#' Returns the empirical distribution, omegas, from empirical likelihood (EL) maximization using an EM algorithm with discontinuitu correction.
 #'
 #' @param G \code{n_obs x n_eqs} matrix of constraints.
 #' @param deltas Length-\code{n_obs} vector of censor indicators, omit if no censoring.
@@ -7,7 +7,7 @@
 #' @param abs_tol Absolute tolerance of Newton-Raphson convergence.
 #' @param verbose Display number of steps and tolerance criterion when algorithm terminates.
 #' @return Length-\code{n_eqs} vector for the resulting empirical distribution, omegas, if the optimization algorithm converged; 1/n_obs if did not converge.
-#' @details The inner-loop optimization of EL is ...
+#' @example examples/omega_hat_EM_smooth.R
 #' @export omega_hat_EM_smooth
 omega_hat_EM_smooth <- function(G, deltas, epsilons, sp=10, max_iter = 100, 
                                 rel_tol = 1e-5, abs_tol = 1e-3, support = FALSE, verbose = FALSE) {
---
+++ examples/omega_hat_EM_smooth.R
@@ -0,0 +1,17 @@
+n <- 20
+p <- 7
+G <- matrix(rnorm(n*p), n, p)
+deltas <- rep(1,n)
+numcens <- sample(round(n/2),1)
+censinds <- sample(n,numcens)
+deltas[censinds] <- 0
+epsilons <- rnorm(n)
+s <- 10
+max_iter <- 500
+rel_tol <- 1e-5
+omega_hat_EM_smooth(G, deltas, epsilons, s, 
+                    max_iter = max_iter, rel_tol = rel_tol, abs_tol = rel_tol, 
+                    support = FALSE, verbose = FALSE) # without support correction
+omega_hat_EM_smooth(G, deltas, epsilons, s, 
+                    max_iter = max_iter, rel_tol = rel_tol, abs_tol = rel_tol, 
+                    support = TRUE, verbose = FALSE) # with support correction
