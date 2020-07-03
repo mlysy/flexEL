594767fdc95f74e2ae4d508c545c15feb2748afa
Shimeng Huang <dora.huang.sunshine@gmail.com>
2020-07-03 13:08:52 -0400

    Updated doxygen main page.

--- src/mainpage.h
+++ src/mainpage.h
@@ -12,7 +12,7 @@
 * flexEL is a computationally efficient library in C++, which has a flexible framework for users to solve any type of regression problems with minimum programming effort.
 *
 * @section struc Code Structure
-* flexEL is currently capable to work with regression problems given fully observed covariates and fully observed or right-censored time-to-event resposes. The corresponding computations are handled separately by the template classes `el::InnerEL` and `el::InnerELC` respectively. These two classes are designed as base classes that will inherit from a regression model class which could be user defined.
+* flexEL is currently capable to work with regression problems given fully observed covariates and fully observed or right-censored time-to-event resposes. The corresponding computations are handled separately by the template classes `flexEL::InnerEL` and `flexEL::InnerELC` respectively. These two classes are designed as base classes that will inherit from a regression model class which could be user defined.
 * Two examples of regression problems, mean regression and quantile regression, are provided in this package, defined by the classes `MeanRegModel` and `QuantRegModel` respectively.
 *
 * @todo Add a link to vignettes tutorials .
