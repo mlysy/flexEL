e25a7283dfd0c491b3bbc89de1a737555668e69a
mlysy <mlysy@users.noreply.github.com>
2020-08-18 08:06:04 -0400

    updated doxygen link

--- inst/doxygen/mainpage.h
+++ inst/doxygen/mainpage.h
@@ -11,7 +11,7 @@
 * @section intro Introduction
 * flexEL is a computationally efficient library in C++, which has a flexible framework for users to solve any type of regression problems with minimum programming effort. 
 * 
-* More details of the framework can be found <a href="https://htmlpreview.github.io/?https://github.com/mlysy/flexEL/blob/develop-api/vignettes/flexEL.html">here</a>.
+* More details of the framework can be found <a href="https://htmlpreview.github.io/?https://github.com/mlysy/flexEL/doc/flexEL.html">here</a>.
 * 
 * @section struc Code Structure
 * flexEL is currently capable to work with regression problems given fully observed covariates and fully observed or right-censored time-to-event resposes. The corresponding computations are handled separately by the template classes `flexEL::InnerEL` and `flexEL::InnerELC` respectively. These two classes are designed as base classes that will inherit from a regression model class which could be user defined.
