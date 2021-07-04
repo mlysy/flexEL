3011da54baae1ca2ea370e2a35e86e01dd0e9621
Shimeng Huang <dora.huang.sunshine@gmail.com>
2021-07-04 14:50:18 -0400

    Tested dGdt for mr model.

--- tests/testthat/test-mr_evalG.R
+++ tests/testthat/test-mr_evalG.R
@@ -22,3 +22,27 @@ test_that("mr_evalG_R == mr_evalG_cpp", {
   }
 })
 
+reorg_jacobian <- function(jac, n, p) {
+  ll <- split(split(jac, row(jac)), rep(seq(from = 1, to = n), p))
+  lm <- lapply(ll, function(l) {
+    matrix(unlist(l), nrow = p)
+  })
+  do.call(rbind, lm)
+}
+
+test_that("dGdt matches numDeriv's jacobian", {
+  for(ii in 1:ntest) {
+    # Location model + mean regression
+    n <- sample(10:20,1)
+    p <- sample(1:(n-2), 1)
+    X <- replicate(p, rnorm(n))
+    beta0 <- rnorm(p)
+    y <- c(X %*% beta0) + rnorm(n) # with N(0,1) error term
+    dGdt_cpp <- flexEL:::.MeanRegEvaldGdt(y, t(X), beta0)
+    mr_evalG_fix <- function(bb) {
+      flexEL::mr_evalG(y, X, bb)
+    }
+    dGdt_R <- numDeriv::jacobian(mr_evalG_fix, beta0, method.args = list(eps = 1e-5))
+    expect_equal(dGdt_cpp, reorg_jacobian(dGdt_R, n, p))
+  }
+})
